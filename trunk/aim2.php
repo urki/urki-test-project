7d3f3519b2886c920c6533c7cc4e0dc144859cfd
uros.gabrovec@gmail.com <uros.gabrovec@gmail.com@06e3adca-2042-b7d1-525d-e2e8a07d12a6>
2011-08-31 07:11:19 +0000

    odstanil query

--- trunk/aim2.php
+++ trunk/aim2.php
@@ -22,7 +22,8 @@ $styleHidden = "class='hide'";
 
 //print_r($person_data['id_person']);
 if ($_REQUEST['prikazi']) {
-    $person_data=$dal->get_person_data_from_persons_by_person_id($name_drop);
+    $name_drop=3;
+$person_data=$dal->get_person_data_from_persons_by_person_id($name_drop);
     $person_data=$person_data[0];
     $first=$person_data[first];
     $last=$person_data[last];
